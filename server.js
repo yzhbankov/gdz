5b5e1e287f1ea02bf7f642318c3b86c8e07eacc7
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-02-12 12:53:09 +0200

    add menu

--- server.js
+++ server.js
@@ -8,16 +8,12 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/gdz';
 
 app.use("/", express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-    if (!req.session.user) {
-        res.render('index.jade', {});
-    } else {
-        res.render('index.jade');
-    }
+    res.render('index.jade');
 });
 
 app.listen(process.env.PORT || 3000, function () {
